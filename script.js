dbffdba8350ad1cad4fbd1b87e645cdba32b3e1d
AlexGh27 <alexghioca27@gmail.com>
2022-09-26 10:49:25 +0300

    Changed the logic again, added the array and array combinations, no visual change

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-
-const gameBoard = (() => {
-  let board = [];
-  return {board};
-})();
-
-document.querySelectorAll('.square').forEach(square => {
-  square.addEventListener('click', function(e) {
-    gameBoard.board.push("X");
-    console.log(gameBoard.board);
-  })
-})
-  
-
-
-
-
-
-const displayController = (() => {
-
-})
-
 const createPlayer = (name, symbol) => {
-
   return {name, symbol}
 }
 
-const alex = createPlayer("Alex", "X");
-const ghioca = createPlayer("Ghioca", "O");
+const gameContainer = () => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+  return {board};
+}
+
+const gameBoard = gameContainer();
+
+
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5], 
+  [6, 7, 8],
+  [0, 3, 6], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
+let counter = 0;
+
+
+
+let square = document.querySelectorAll(".square");
+square.forEach(square => {
+  square.addEventListener("click", function(e) {
+    let boardString = square.id.substring(1);
+    let boardIndex = Number(boardString);
+    gameBoard.board[boardIndex] = symbolChooser();
+    console.log(gameBoard.board);
+    counter++;
+    checkWinner();
+  }, {once: true})
+})
+
+
+function symbolChooser() {
+  if (counter % 2 === 0) {
+    return "X";
+  }
+  else {
+    return "0";
+  }
+}
+
+function checkWinner() {
+  for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+    const combination = WINNING_COMBINATIONS[i];
+    const firstCell = gameBoard.board[combination[0]];
+    const secondCell = gameBoard.board[combination[1]];
+    const thirdCell = gameBoard.board[combination[2]];
+
+    if (firstCell == "" || secondCell == "" || thirdCell == "") {
+      continue;
+    }
+    else if (firstCell == secondCell && secondCell == thirdCell) {
+      break;
+    }
+  }
+}
+
+
+
+
+
 
